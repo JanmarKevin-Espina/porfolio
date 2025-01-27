e2513a426e07e82eb2cde2d712bedaaff8229e84
Janmark <janmarkespina.yns@gmail.com>
2025-01-28 05:54:55 +0800

    latest

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+(function ($) {
+
+  "use strict";
+
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+    })
+
+    // HEADER
+    $(".navbar").headroom();
+
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+    	items: 1,
+	    loop:true,
+	    margin:10,
+	    nav:true
+	});
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.nav-link, .custom-btn-link').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+        }, 1000);
+        event.preventDefault();
+      });
+    });
+
+    $(function () {
+      // Automatically update the year using jQuery
+      const currentYear = new Date().getFullYear();
+      $('#year').text(currentYear);
+    });
+
+    // TOOLTIP
+    $('.social-links a').tooltip();
+
+})(jQuery);
